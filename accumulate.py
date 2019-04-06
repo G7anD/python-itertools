2fba5025bbe450012781289274a654a2d39501d4
I.Erkin <Grandboy99.ei@gmail.com>
2019-04-06 11:32:15 +0500

    Update accumulate.py

--- accumulate.py
+++ accumulate.py
@@ -6,9 +6,7 @@ numbers = [3, 2, 1, 5, 3, 4, 5]
 
 nat = it.accumulate(numbers)
 
-print(list(nat))
-# [3, 5, 6, 6, 9, 13, 18]
+print(list(nat))  # [3, 5, 6, 6, 9, 13, 18]
 
 nat2 = it.accumulate(numbers, op.mul)
-print(list(nat2))
-# [3, 6, 6, 30, 90, 360, 1800]
+print(list(nat2))  # [3, 6, 6, 30, 90, 360, 1800]
